66390618e64ac010c087ad465a68e4b67675a118
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-05-19 12:41:02 -0700

    Change hash table to delegator (OLOO)

--- __tests__/hashTable.test.js
+++ __tests__/hashTable.test.js
@@ -1,34 +1,77 @@
 const { HashTable } = require( '../structs' );
 
+// Mock data
 const size = 11,
-  mockTable = new HashTable( size ),
-  keys = [12, 13, 25, 29, 31, 30],
-  names = ['john', 'mary', 'bob', 'eric', 'amy', 'zeke'];
+  mockTable = Object.create( HashTable ),
+  keys = [12, 13, 25, 29, 31, 27],
+  names = [`john`, `mary`, `bob`, `eric`, `amy`, `zeke`];
 
-describe( 'hash table', () => {
-  it( 'throws without prime size', () => {
-    expect( () => new HashTable( 4 ) ).toThrow( /Choose a prime number/ );
+describe( `Hash Table delegator`, () => {
+  it( 'initalizes', () => {
+    mockTable.init( size );
+    expect( mockTable.size ).toEqual( size );
+    expect( mockTable.keys.length ).toEqual( size );
+    expect( mockTable.values.length ).toEqual( size );
+    expect( mockTable.items ).toEqual( 0 );
+
+    const proto = Object.getPrototypeOf( mockTable );
+    expect( proto ).toHaveProperty( `hash` );
+    expect( proto ).toHaveProperty( `getItem` );
+    expect( typeof proto.getItem ).toBe( `function` );
+    expect( proto ).toHaveProperty( `setItem` );
+    expect( typeof proto.setItem ).toBe( `function` );
   } );
 
-  it( 'hashes and puts data', () => {
-    // Input mock data to hashtable
+  it( `sets items`, () => {
+    // Ensure mock keys/names correct
     expect( keys.length ).toEqual( names.length );
+    // Input mock data to hashtable
     for ( let i = 0; i < keys.length; i++ ) {
-      mockTable.put( keys[i], names[i++] );
+      mockTable.setItem( keys[i], names[i] );
     }
-    expect( mockTable.size ).toEqual( size );
+    // Sets keys
+    expect( mockTable.keys ).toContain( 13 );
+    expect( mockTable.keys ).toContain( 31 );
+    // Sets values
     expect( mockTable.values ).toContain( names[0] );
     expect( mockTable.values ).toContain( names[2] );
     expect( mockTable.values ).toContain( names[4] );
   } );
 
-  it( 'gets set with key', () => {
-    const john = mockTable.get( 12 );
-    const bob = mockTable.get( 25 );
-    const amy = mockTable.get( 31 );
+  it( `gets items given original key`, () => {
+    const john = mockTable.getItem( 12 );
+    const bob = mockTable.getItem( 25 );
+    const amy = mockTable.getItem( 31 );
 
-    expect( john ).toBe( 'john' );
-    expect( bob ).toBe( 'bob' );
-    expect( amy ).toBe( 'amy' );
+    expect( john ).toBe( `john` );
+    expect( bob ).toBe( `bob` );
+    expect( amy ).toBe( `amy` );
+  } );
+
+  it( `resolves collisions`, () => {
+    // 29 = hashedKeyIndex[2]
+    expect( mockTable.keys[2] ).toEqual( 29 );
+
+    // hash(51) -> (11-2) - (51 % 11) % (11-2) = 2 -> collision
+    mockTable.setItem( 51, `collider` );
+    expect( mockTable.keys ).toContain( 51 );
+    expect( mockTable.keys[3] ).toEqual( 51 );
+  } );
+
+  it( `allows duplicates without overwrites`, () => {
+    // Create another table
+    const anotherTable = Object.create( HashTable );
+    const differentSize = 7;
+    anotherTable.init( differentSize );
+
+    // Check each size
+    expect( mockTable.size ).toEqual( size );
+    expect( anotherTable.size ).toEqual( differentSize );
+
+    // Ensure no interactions between tables
+    const anotherName = `juanita`;
+    anotherTable.setItem( 18, anotherName );
+    expect( mockTable.getItem( 18 ) ).toBe( undefined );
+    expect( anotherTable.getItem( 18 ) ).toBe( anotherName );
   } );
 } );
--- structs/hashTable.js
+++ structs/hashTable.js
@@ -1,5 +1,5 @@
 const isInteger = require( 'lodash/isInteger' );
-const isNull = require( 'lodash/isNull' );
+const isNil = require( 'lodash/isNil' );
 const { isPrime } = require( '../utils' );
 
 /**
@@ -14,67 +14,62 @@ const { isPrime } = require( '../utils' );
  * Use quadratic probing or double hashing to avoid collisions
  * Linear probing with no second hash can result in clusters
  *
+ * Example of hash table in use: localStorage
+ *
  */
 
-function HashTable( size ) {
-  this.size = size;
-  this.keys = this.init( size );
-  this.values = this.init( size );
-  this.limit = 0;
-}
+module.exports = {
+  init( size ) {
+    if ( isPrime( size ) ) {
+      this.size = size;
+      this.keys = Array( size ).fill();
+      this.values = Array( size ).fill();
+      this.items = 0;
+    } else {
+      throw Error( `Choose a prime number size to avoid collisions` );
+    }
+  },
+  hash( key ) {
+    if ( isInteger( key ) ) {
+      // Base hash
+      const firstHash = key % this.size;
+      // Double hash
+      // hash2(x) = R - (hash1() % R)
+      // where R is less than this.size
+      const R = this.size - 2;
+      return R - ( firstHash % R );
+    }
+  },
+  getItem( key ) {
+    if ( !~this.keys.indexOf( key ) ) {
+      return; // undefined
+    }
 
-// Intialize k/v array
-HashTable.prototype.init = function( size ) {
-  if ( !isPrime( size ) ) {
-    throw Error( 'Choose a prime number size to avoid collisions.' );
-  }
-  return Array( size ).fill( null );
+    let hashedIdx = this.hash( key );
+
+    while ( this.keys[hashedIdx] !== key ) {
+      // Simple linear (use quadratic to avoid clusters)
+      hashedIdx = ++hashedIdx % this.size;
+    }
+    return this.values[hashedIdx];
+  },
+  setItem( key, val ) {
+    if ( this.items >= this.size ) {
+      throw Error( `Hash table is full!` );
+    }
+    if ( !isInteger( key ) ) {
+      throw Error( `Key must be an integer` );
+    }
+
+    // Determine the (unique) hash index
+    let hashedIdx = this.hash( key );
+    while ( !isNil( this.keys[hashedIdx] ) ) {
+      hashedIdx = ++hashedIdx % this.size;
+    }
+
+    // Add the new item
+    this.keys[hashedIdx] = key;
+    this.values[hashedIdx] = val;
+    this.items++;
+  },
 };
-
-// Base hash function
-HashTable.prototype.hash = function( k ) {
-  if ( !isInteger( k ) ) {
-    throw Error( `Key must be an integer` );
-  }
-
-  const firstHashedKey = k % this.size;
-  return this.secondHash( firstHashedKey );
-};
-
-HashTable.prototype.secondHash = function( firstHashedKey ) {
-  // hash2(x) = R - (hash1() % R)
-  const R = this.size - 2;
-  return R - ( firstHashedKey % R );
-};
-
-// Setter O(1) - Double hash w/ linear probing
-HashTable.prototype.put = function( k, v ) {
-  if ( this.limit >= this.size ) {
-    throw Error( `Hash table is full` );
-  }
-
-  let hashedIdx = this.hash( k );
-
-  while ( !isNull( this.keys[hashedIdx] ) ) {
-    hashedIdx++;
-    hashedIdx = hashedIdx % this.size;
-  }
-
-  this.keys[hashedIdx] = k;
-  this.values[hashedIdx] = v;
-  this.limit++;
-};
-
-// Getter O(1) - Double hash w/ linear probing
-HashTable.prototype.get = function( k ) {
-  let hashedIdx = this.hash( k );
-
-  while ( this.keys[hashedIdx] != k ) {
-    hashedIdx++;
-    hashedIdx = hashedIdx % this.size;
-  }
-
-  return this.values[hashedIdx];
-};
-
-module.exports = HashTable;
