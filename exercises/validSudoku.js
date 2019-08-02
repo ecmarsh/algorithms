ef12be5b6087a83745848ebd6e39abfae1d41653
ethan-marsh <emarsh55@gmail.com>
2019-08-02 16:55:18 -0700

    Is Valid Sudoku

--- exercises/validSudoku.js
+++ exercises/validSudoku.js
@@ -8,14 +8,14 @@
  * Each column must contain the digits 1-9 w/o repetition.
  * Each of the 9 3x3 sub-boxes of the grid must contain 1-9 w/o repetition.
  *
- * The Sudoku board could be partially filled, 
+ * The Sudoku board could be partially filled,
  * where empty cells are filled with the character '.'.
  *
  * @example
  *
  * Input:
  * [
- *  ["5","3",".",".","7",".",".",".","."],
+ *    ["5","3",".",".","7",".",".",".","."],
  *    ["6",".",".","1","9","5",".",".","."],
  *    [".","9","8",".",".",".",".","6","."],
  *    ["8",".",".",".","6",".",".",".","3"],
@@ -55,13 +55,13 @@ module.exports = function isValidSudoku( board ) {
       const sqrt = Math.sqrt( board.size );
       const b = ( r / sqrt | 0 ) + sqrt * ( c / sqrt | 0 );
 
-      if ( num in boxes[ b ] ) {
+      if ( num in boxes[b] ) {
         return false;
       }
-      if ( num in rows[ r ] ) {
+      if ( num in rows[r] ) {
         return false;
       }
-      if ( num in cols[ c ] ) {
+      if ( num in cols[c] ) {
         return false;
       }
 
