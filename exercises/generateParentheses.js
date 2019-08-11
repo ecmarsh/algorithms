da7099cfc7e4384760a2fcc1b5a3a3d6666bcf34
ethan-marsh <emarsh55@gmail.com>
2019-08-10 19:29:03 -0700

    Gen parens (Backtrack/DFS)

---
+++ exercises/generateParentheses.js
@@ -0,0 +1,62 @@
+/**
+ * Generate Parentheses
+ *
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ */
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+module.exports = function generateParentheses( n ) {
+  if ( !n ) {
+    return [];
+  }
+
+  if ( n === 1 ) {
+    return ['()'];
+  }
+
+  const combinations = [],
+    [left, right] = ['(', ')'];
+
+  placeParen( [], n, n );
+
+  function placeParen( combo, lefts, rights ) {
+    // Base case: lefts and rights all used
+    if ( !lefts && !rights ) {
+      combinations.push( combo );
+      return;
+    }
+
+    // Constructors: add left if available, close if open
+    if ( lefts ) {
+      placeParen( combo+left, lefts-1, rights );
+    }
+    if ( lefts < rights ) {
+      placeParen( combo+right, lefts, rights-1 );
+    }
+  }
+
+  return combinations;
+};
+
+/*
+
+n=3
+
+Goal:
+  Place 3 lefts, '('
+  Place 3 rights, ')'
+
+Well-formed parentheses:
+  1. right must have a corresponding left occuring before
+     - lefts < rights to place right
+
+  2. all lefts must be closed with a right
+     - lefts & rights === 0 denotes end
+
+LR= 33-> [23]-> [13,22]  -> [ 03, 12 ]  ->  [ 02, 11 ]      -> [ 01 ]-> 00
+p = ''-> '('->['((','()']->['(((','()(']->['((()', '()()']-> '((()))','()()()'
+
+*/
