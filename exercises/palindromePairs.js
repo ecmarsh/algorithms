e8dd2f69dcea24ae70dc018b0e4c940e2165012c
ethan-marsh <emarsh55@gmail.com>
2019-08-17 19:29:19 -0700

    Group palindrome substrings (backtracking)

---
+++ exercises/palindromePairs.js
@@ -0,0 +1,31 @@
+/**
+ * Palindrome Pairs
+ *
+ * Given a list of _unique_ words, find all pairs of
+ * _distinct_ indices (i,j) in the list, so that the concatenation,
+ * of the two words (words[i] + words[j]) is a palindrome.
+ *
+ * @example
+ * Input: ['abcd','dcba','lls','s','sssll']
+ * Output: [[0,1][1,0][3,2][2,4]]
+ * Explanation: The palindromes are:
+ * ['dcbaabcd','abcddcba','slls','llsssll']
+ *
+ * @example
+ * Input: ['bat','tab','cat']
+ * Output: [[0,1],[1,0]]
+ * Explanation: ['battab','tabbat']
+ *
+ * Analysis:
+ * Time: O()
+ * Space: O()
+ *
+ */
+
+/**
+ * @param {string[]} words
+ * @return {number[][]}
+ */
+module.exports = function palindromePairs( words ) {
+
+};
