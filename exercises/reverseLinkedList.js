987d8d0cb1a65fe0c1684158f37f705b054f62da
ethan-marsh <emarsh55@gmail.com>
2019-09-09 18:16:23 -0700

    Add reverse list recursive solution

--- exercises/reverseLinkedList.js
+++ exercises/reverseLinkedList.js
@@ -10,7 +10,7 @@
  * @param {ListNode} head The starting head
  * @return {ListNode} The new head
  */
-module.exports = function reverseSLLIterative( head ) {
+module.exports.iterative = function reverseListIterative( head ) {
   let prev = null;
   let cur = head;
 
@@ -74,6 +74,67 @@ LOOP 4: (cur=4, prev=3)
 return new head = last cur = prev = 4
 
 4->3->2->1->null
-
 */
 
+
+module.exports.recursive = function reverseListRecursive( head ) {
+  // Base case only reached if no head for initial call
+  // Or when we've reached the tail (aka head.next = null)
+  if ( head === null || head.next === null ) {
+    return head;
+  }
+
+  const tail = reverseListRecursive( head.next ); // Will call until tail
+  head.next.next = head; // This iniates reversal, but creates cycle
+  head.next = null; // This breaks cycle, putting head as current tail
+  return tail; // Tail is the head in reversed
+};
+
+/*
+PSUEDO (recursive):
+We recursively traverse to get to tail,
+keeping refs in each frame and reverse backwards:
+
+1. If no head/node (initial call) or reached tail (node.next=null):
+        return head/node
+2. tail=traverse next (this will get us to tail and always return tail)
+
+Now working backwards (popping off stack):
+  3. set cur.next.next to point to cur
+  4. set cur.next to null (important, or else cycle)
+
+Ex: 1->2->3->4
+Recursive will get us to tail=4
+Note in all calls, prev is always the tail.
+
+Reversal 1: (cur=3, prev=4)
+ a. (cur.next.next = prev)
+    cur.next is 4
+    4.next = 3 (cyclical now)
+ b. cur.next = null ( break cycle)
+    3.next = null (break cycle)
+    1->2->3->null
+ c. return prev=4(->3->null) to next call
+
+Reversal 2: (cur=2, prev=4->3->null)
+ a. (cur.next.next = prev)
+    cur.next is 3
+    3.next = 2
+ b. cur.next = null
+    2.next = null
+    1->2->null
+ c. return prev=4(->3->2->null) to next call
+
+Reversal 3: (cur=1, prev=4->3->2->null)
+ a. (cur.next.next = prev)
+    cur.next is 2
+    2.next = 1
+ b. cur.next = null
+    1.next = null
+    ...1->null
+ c. return prev=4->3->2->1->null to next call
+          last on stack -> ans
+
+Note this solution is tricker and uses O(N) space compared to iterative
+
+*/
