8ada2b9ba399f1461bb85d7263633e79de16b2fa
Ethan Marsh <emarsh55@gmail.com>
2019-09-24 19:47:11 -0700

    Max rectangle optimal solution using histogram as dp table

--- exercises/maxRectangle.js
+++ exercises/maxRectangle.js
@@ -14,11 +14,69 @@
  * ]
  * Output: 6
  *
+ * Analysis (Optimal Solution)
+ * N = rows (matrix.length). M = cols (matrix[i].length) where 0 <= i < N
+ * Time: O(N * (M + M)) = O(N * M)
+ * Space: O(M) Heights histogram for each col.
+ *
  */
 
 /**
+ * @param {string[][]} matrix
+ * @return {number}
+ */
+module.exports.optimal = function maxRectangle( matrix ) {
+  if ( !matrix || !matrix.length || !matrix[0].length ) {
+    return 0;
+  }
+
+  const dp = Array( matrix[0].length ).fill( 0 );
+  let maxArea = 0;
+
+  for ( let r = 0; r < matrix.length; r++ ) {
+    for ( let c = 0; c < matrix[0].length; c++ ) {
+      dp[c] = matrix[r][c] === '1' ? dp[c] + 1 : 0;
+    }
+    maxArea = Math.max( maxArea, getMaxFromHistogram( dp ) );
+  }
+
+  return maxArea;
+};
+
+/**
+ * See https://leetcode.com/problems/largest-rectangle-in-histogram/
+ * @param {number[]} heights
+ * @return {number}
+ */
+function getMaxFromHistogram( heights ) {
+  const s = [-1];
+  s.peek = () => s[s.length -1];
+
+  let maxArea = 0;
+  const updateMaxArea = ( col ) => {
+    const height = heights[s.pop()];
+    const width = col - s.peek() - 1;
+    maxArea = Math.max( maxArea, width * height );
+  };
+
+  heights.forEach( ( height, i ) => {
+    while ( s.peek() !== -1 && heights[s.peek()] >= height ) {
+      updateMaxArea( i );
+    }
+    s.push( i );
+  } );
+
+  // Repeat for final row
+  while ( s.peek() !== -1 ) {
+    updateMaxArea( heights.length );
+  }
+
+  return maxArea;
+}
+
+/**
  * Brute Force Analysis:
- * N = rows (matrix.length). M = cols (matrix[i].length) where 0 <= i < N
+ * _See optimal analysis variable definitions_
  * Time: O(N^2 * M) Standard matrix traversal with addition of second row pass
  * Space: O(N * M) DP table with max widths.
  */
@@ -27,10 +85,10 @@ module.exports.bruteForce = function maxRectangleBrute( matrix ) {
     return 0;
   }
 
-  let maxArea = 0;
   const rows = matrix.length;
   const cols = matrix[0].length;
   const dp = Array( rows ).fill( 0 ).map( ( _ ) => Array( cols ).fill( 0 ) );
+  let maxArea = 0;
 
   for ( let r = 0; r < rows; r++ ) {
     for ( let c = 0; c < cols; c++ ) {
