f0c2ece0ff390f31cee98d028105d8fdf2e5c7fd
Ethan Marsh <emarsh55@gmail.com>
2019-10-19 12:53:46 -0700

    Uppercase literal consts

--- exercises/mergeIntervals.js
+++ exercises/mergeIntervals.js
@@ -9,21 +9,19 @@
  */
 
 module.exports = function mergeIntervals( intervals ) {
-  // 0. Handle edge cases
   if ( !intervals || !intervals.length ) {
     return [];
   }
 
-  // Using more descriptive names
-  const start = 0, end = 1;
+  const BEG = 0;
+  const END = 1;
 
   // 1. Sort the intervals based on increasing order of starting time.
-  intervals.sort( ( a, b ) => a[start] - b[start] );
+  intervals.sort( ( a, b ) => a[BEG] - b[BEG] );
 
-  // 2. Push the first interval on to a 'stack'.
+  // 2. Push the first interval on to stack
   const Stack = function( initialInterval ) {
     const intervals = [initialInterval];
-
     return {
       intervals,
       get mergedIntervals() {
@@ -36,28 +34,27 @@ module.exports = function mergeIntervals( intervals ) {
         return this.intervals[this.intervals.length - 1];
       },
       set updatedIntervalEnd( newEndTime ) {
-        this.intervals[this.intervals.length - 1][end] = newEndTime;
+        this.intervals[this.intervals.length - 1][END] = newEndTime;
       },
     };
   };
 
   const s = new Stack( intervals[0] );
 
-  // 3. For each interval do the following
+  // 3. For each interval, do:
   intervals.forEach( interval => {
     // a. If the current interval does not overlap with the stack top, push it.
-    if ( interval[start] > s.top[end] ) {
+    if ( interval[BEG] > s.top[END] ) {
       s.push( interval );
     }
-
     // b. If the current interval overlaps with stack top and
-    // ending time of current interval is more than that of stack top,
-    // update stack top with the ending time of current interval.
-    else if ( interval[end] > s.top[end] ) {
-      s.updatedIntervalEnd = interval[end];
+    // ENDing time of current interval is more than that of stack top,
+    // update stack top with the ENDing time of current interval.
+    else if ( interval[END] > s.top[END] ) {
+      s.updatedIntervalEnd = interval[END];
     }
   } );
 
-  // 4. At the end stack contains the merged intervals.
+  // 4. Stack contains the merged intervals.
   return s.mergedIntervals;
 };
