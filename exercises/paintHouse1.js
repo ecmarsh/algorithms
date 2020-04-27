ab0ac8c9c44cf0982137cab1e89f94faf892de42
Ethan Marsh <emarsh55@gmail.com>
2020-04-27 14:31:53 -0700

    exercises(paintHouse1): add recursive with memoization solution

--- exercises/paintHouse1.js
+++ exercises/paintHouse1.js
@@ -23,10 +23,18 @@
  *  Paint house 0 into blue, paint house 1 into green, paint house 2 into blue.
  *  Minimum cost: 2 + 5 + 3 = 10.
  *
- * @complexity
- * Time:
- * Space:
+ * @analysis
+ * Brute Force: generate all permutations of house colors then remove invalid ones that have two side by side. From there, find the one with the lowest cost. O(3^n) runtime.
+ *
+ * Brute Force w Recursion: Calculate the min cost at each level using a tree where root is the previous house that has two children, the other color combinations of the next house. At each level, take the min cost of its two children (bottom up). At the end, root node, is the answer. Time is O(2^n), the total number of permutations and O(n) space for run-time stack w recursion.
+ *
+ * Recursion with memoization: Recursive, but memoize the repeated calculations with a map, cutting down the time to O(n) since there are 3 colors -> 3*n sets of parameters so can't be more than 3*2*n searches in memoization. Paint cost run time is O(1) as a conditional, so 6*n -> O(n). Space becomes 3*2*n + n -> 12n -> O(n).
+ *
+ * Dynamic Programming: TODO
  */
+
+const COLORS = [0, 1, 2];
+// const [RED, GREEN, BLUE] = colors;
 
 /**
  * @param {number[][]} costs
@@ -35,3 +43,40 @@
 module.exports = function minCost( costs ) {
   // TODO
 };
+
+
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+module.exports.recursiveWithMemo = function minCostRecursive( costs ) {
+  if ( !costs || !costs.length ) return 0;
+
+  // Houses where min cost with current color already calculated
+  const memo = new Map();
+
+  // Returns a stringed key from houseIndex and chosen color
+  const houseColorKey = ( houseIndex, color ) => `${houseIndex}${color}`;
+
+  const paintCost = function ( houseIndex, color ) {
+    const k = houseColorKey( houseIndex, color );
+    if ( memo.has( k ) ) return memo.get( k );
+
+    let totalCost = costs[houseIndex][color];
+    const isLastHouse = houseIndex === costs.length - 1;
+
+    if ( !isLastHouse ) {
+      // Colors that aren't the color of the previous house color
+      const colorOptions = COLORS.filter( ( c ) => c !== color );
+      const nextHouse = houseIndex + 1;
+      totalCost += Math.min( ...colorOptions.map( ( c ) => paintCost( nextHouse, c ) ) );
+    }
+
+    memo.set( k, totalCost );
+    return totalCost;
+  };
+
+  const startHouse = 0;
+  return Math.min( ...COLORS.map( ( color ) => paintCost( startHouse, color ) ) );
+};
+
