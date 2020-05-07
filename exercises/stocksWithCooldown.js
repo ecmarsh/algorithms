7bfe83e43bfcf5c855c63d3dd1549f3af101bd3d
Ethan Marsh <emarsh55@gmail.com>
2020-05-06 17:58:16 -0700

    exercise: add stocks with cooldown description (medium)

---
+++ exercises/stocksWithCooldown.js
@@ -0,0 +1,33 @@
+/**
+ * @lc id=309 lang=javascript tag=dp
+ *
+ * [309] Best Time to Buy/Sell Stocks with Cooldown
+ *
+ * Say you have an array for which the ith element is the price
+ * of a given stock on day i.
+ *
+ * Design an algorithm to find the maximum profit. You may complete
+ * as many transactions as you like (ie, buy one and sell one
+ * share of the stock multiple times) with the following restrictions:
+ *   - You may not engage in multiple transactions at the same time
+ *    (ie, you must sell the stock before you buy again).
+ *   - After you sell your stock, you cannot buy stock on next day.
+ *     (ie, cooldown 1 day)
+ *
+ * @example
+ * Input: [1,2,3,0,2]
+ * Output: 3
+ * Explanation: transactions = [buy, sell, cooldown, buy, sell]
+ *
+ * @complexity
+ * Time: O()
+ * Space: O()
+ */
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+module.exports = function maxProfit( prices ) {
+
+};
