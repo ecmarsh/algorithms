fb41e47a4d1215610dacef042d6a4c422e753199
Ethan Marsh <emarsh55@gmail.com>
2020-05-10 13:07:34 -0700

    notes(isPerfectSquare): show work for newtons method

--- exercises/isPerfectSquare.js
+++ exercises/isPerfectSquare.js
@@ -63,3 +63,55 @@ module.exports.newtons = function ( num ) {
   }
   return ( x * x == num );
 };
+
+/*
+
+Newton's method
+f(x) = num - x^2 === 0
+
+Use tangent line:
+
+x2 = x1 - dx
+slope(derivative/f'(x)) = f(x1) / dx
+  - dx is difference of tangent line vs guess
+
+dx = f(x1) / slope
+dx = f(x1) / f'(x1)
+
+x2 = x1 - dx
+
+So...
+x_(k+1) = x_k - dx
+x_(k+1) = x_k - f(x_k) / f'(x_k)
+
+f(x) = num - x^2 === 0
+f'(x) = (f(x-dx) - f(x)) / dx
+  f(x-dx) = num - (x-dx)^2 === 0
+  f(x-dx) - f(x) =  num - (x-dx)^2 - (num - x^2)
+  num - (x^2 - 2dx*x + dx^2) - (num - x^2)
+  num - x^2 + 2dx*x + dx^2 - num + x^2
+f'(x) = (2dx*x + dx^2) / dx
+f'(x) = dx(2x+dx) / dx
+f'(x) = 2x+dx -> as dx heads towards 0, we get 2x
+f'(x) = 2x
+
+Finding...
+f(x) = num-x^2 = 0
+f'(x) = 2x
+
+Thus...
+x_k+1 = x_k - f(x_k) / f'(x_k)
+x_k+1 = x_k - (num - xk^2) / (2xk)
+x_k+1 = x_k - 1/2*((num-xk^2)/x_k)
+
+Then need to guess...
+f(x) = num - x^2 is monotonous, so want to start with smallest seed -> num / 2
+
+Keep incrementing k until x*x is less than or equal to the number. If it's less than, its not a valid square but if it equals, then we found the root.
+
+So starting with num/2 up until the guess (x*x) is greater than the number,
+keep guessing. Then it's true if the guess is the number.
+
+Proof of O(log n) convering quadratically: https://en.wikipedia.org/wiki/Newton%27s_method#Proof_of_quadratic_convergence_for_Newton's_iterative_method
+
+*/
